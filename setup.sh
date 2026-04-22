8890e66e9d33ffe04973ac8f3fe611e01dfdb35c
ha1dtd <haidtd0308@gmail.com>
2026-04-22 09:37:17 +0700

    22/4

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-echo "=== BASIC INPUT ==="
+echo "=== FOXAI CLUSTER SETUP ==="
 
+# --- USER INPUT ---
 read -p "Namenode PUBLIC IP: " NN_PUBLIC_IP
 read -p "Namenode PRIVATE IP: " NN_PRIVATE_IP
 read -p "Datanode1 PRIVATE IP: " DN1_PRIVATE_IP
@@ -20,110 +21,116 @@ fi
 
 HADOOP_HOME="$BASE_HOME/hadoop"
 SPARK_HOME="/opt/spark"
+JAVA_HOME="/usr/lib/jvm/temurin-11-jdk-amd64"
 
+# --- SYSTEM DEPENDENCIES ---
 echo "=== INSTALL BASE DEPENDENCIES ==="
 sudo apt update
-sudo apt install -y wget gpg ssh pdsh python3-venv python3-pip
+sudo apt install -y wget gpg ssh pdsh python3-venv python3-pip curl tar
 
+# --- JAVA 11 INSTALL ---
 echo "=== INSTALL JAVA 11 ==="
 wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo gpg --dearmor -o /usr/share/keyrings/adoptium.gpg
 echo "deb [signed-by=/usr/share/keyrings/adoptium.gpg] https://packages.adoptium.net/artifactory/deb bookworm main" | sudo tee /etc/apt/sources.list.d/adoptium.list
 sudo apt update
-sudo apt install temurin-11-jdk -y
+sudo apt install -y temurin-11-jdk
 
-JAVA_HOME=/usr/lib/jvm/temurin-11-jdk-amd64
-
+# --- HADOOP INSTALL ---
 echo "=== INSTALL HADOOP ==="
-cd ~
+cd $BASE_HOME
 wget -4 https://dlcdn.apache.org/hadoop/common/hadoop-3.3.6/hadoop-3.3.6.tar.gz
 tar -xvzf hadoop-3.3.6.tar.gz
 mv hadoop-3.3.6 $HADOOP_HOME
 
+# --- SPARK INSTALL ---
 echo "=== INSTALL SPARK ==="
 wget -4 https://dlcdn.apache.org/spark/spark-3.5.8/spark-3.5.8-bin-hadoop3.tgz
 tar -xvzf spark-3.5.8-bin-hadoop3.tgz
 sudo mv spark-3.5.8-bin-hadoop3 $SPARK_HOME
+sudo chown -R $NN_USER:$NN_USER $SPARK_HOME
 
-echo "=== ENV SETUP ==="
-cat <<EOT >> ~/.bashrc
+# --- ENV SETUP ---
+echo "=== SETUP ENVIRONMENT VARIABLES ==="
+cat <<EOT >> $BASE_HOME/.bashrc
 export JAVA_HOME=$JAVA_HOME
 export HADOOP_HOME=$HADOOP_HOME
 export SPARK_HOME=$SPARK_HOME
-export PATH=\$PATH:\$JAVA_HOME/bin:\$HADOOP_HOME/bin:\$HADOOP_HOME/sbin:\$SPARK_HOME/bin:\$SPARK_HOME/sbin
 export HADOOP_CONF_DIR=\$HADOOP_HOME/etc/hadoop
+export PATH=\$PATH:\$JAVA_HOME/bin:\$HADOOP_HOME/bin:\$HADOOP_HOME/sbin:\$SPARK_HOME/bin:\$SPARK_HOME/sbin
+export HADOOP_SSH_OPTS="-o BatchMode=yes -o StrictHostKeyChecking=no -o ConnectTimeout=10"
+export PDSH_RCMD_TYPE=ssh
 EOT
-source ~/.bashrc
+source $BASE_HOME/.bashrc
 
-echo "=== /etc/hosts ==="
+# --- /etc/hosts ---
+echo "=== CONFIGURE /etc/hosts ==="
 sudo bash -c "cat >> /etc/hosts" <<EOT
 $NN_PRIVATE_IP namenode
 $DN1_PRIVATE_IP datanode1
 $DN2_PRIVATE_IP datanode2
 EOT
 
-echo "=== SSH SETUP ==="
-ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+# --- SSH SETUP ---
+echo "=== SSH KEYS & ACCESS ==="
+ssh-keygen -t rsa -N "" -f $BASE_HOME/.ssh/id_rsa
 ssh-copy-id $DN_USER@$DN1_PRIVATE_IP
 ssh-copy-id $DN_USER@$DN2_PRIVATE_IP
 ssh-copy-id localhost
 
-echo "=== DISTRIBUTE HADOOP ==="
+# --- DISTRIBUTE HADOOP & SPARK ---
+echo "=== DISTRIBUTE HADOOP & SPARK TO DATANODES ==="
 scp -r $HADOOP_HOME $DN_USER@$DN1_PRIVATE_IP:$BASE_HOME/
 scp -r $HADOOP_HOME $DN_USER@$DN2_PRIVATE_IP:$BASE_HOME/
-
-echo "=== DISTRIBUTE SPARK ==="
 scp -r $SPARK_HOME $DN_USER@$DN1_PRIVATE_IP:/opt/
 scp -r $SPARK_HOME $DN_USER@$DN2_PRIVATE_IP:/opt/
 
-echo "=== REMOTE ENV SETUP (DATANODES) ==="
-for node in $DN1_PRIVATE_IP $DN2_PRIVATE_IP
-do
+# --- REMOTE ENV SETUP ON DATANODES ---
+echo "=== SETUP ENVIRONMENT ON DATANODES ==="
+for node in $DN1_PRIVATE_IP $DN2_PRIVATE_IP; do
 ssh $DN_USER@$node <<EOF
 cat <<EOT >> ~/.bashrc
 export JAVA_HOME=$JAVA_HOME
 export HADOOP_HOME=$BASE_HOME/hadoop
 export SPARK_HOME=/opt/spark
-export PATH=\$PATH:\$JAVA_HOME/bin:\$HADOOP_HOME/bin:\$HADOOP_HOME/sbin:\$SPARK_HOME/bin:\$SPARK_HOME/sbin
 export HADOOP_CONF_DIR=\$HADOOP_HOME/etc/hadoop
+export PATH=\$PATH:\$JAVA_HOME/bin:\$HADOOP_HOME/bin:\$HADOOP_HOME/sbin:\$SPARK_HOME/bin:\$SPARK_HOME/sbin
 EOT
 source ~/.bashrc
 EOF
 done
 
-echo "=== CREATE DATA DIR (ALL NODES) ==="
-mkdir -p $HADOOP_HOME/data/namenode
-mkdir -p $HADOOP_HOME/data/datanode
-
-for node in $DN1_PRIVATE_IP $DN2_PRIVATE_IP
-do
-ssh $DN_USER@$node "mkdir -p $BASE_HOME/hadoop/data/namenode && mkdir -p $BASE_HOME/hadoop/data/datanode"
+# --- CREATE DATA DIRS ---
+echo "=== CREATE HADOOP DATA DIRECTORIES ==="
+mkdir -p $HADOOP_HOME/data/namenode $HADOOP_HOME/data/datanode
+for node in $DN1_PRIVATE_IP $DN2_PRIVATE_IP; do
+ssh $DN_USER@$node "mkdir -p $BASE_HOME/hadoop/data/namenode $BASE_HOME/hadoop/data/datanode"
 done
 
-echo "=== HADOOP CONFIG ==="
-
+# --- HADOOP CONFIG ---
+echo "=== HADOOP CONFIGURATION ==="
 cat <<EOT > $HADOOP_HOME/etc/hadoop/core-site.xml
 <configuration>
-<property>
-  <name>fs.defaultFS</name>
-  <value>hdfs://$NN_PRIVATE_IP:9000</value>
-</property>
+  <property>
+    <name>fs.defaultFS</name>
+    <value>hdfs://$NN_PRIVATE_IP:9000</value>
+  </property>
 </configuration>
 EOT
 
 cat <<EOT > $HADOOP_HOME/etc/hadoop/hdfs-site.xml
 <configuration>
-<property>
-  <name>dfs.replication</name>
-  <value>2</value>
-</property>
-<property>
-  <name>dfs.namenode.name.dir</name>
-  <value>file://$HADOOP_HOME/data/namenode</value>
-</property>
-<property>
-  <name>dfs.datanode.data.dir</name>
-  <value>file://$HADOOP_HOME/data/datanode</value>
-</property>
+  <property>
+    <name>dfs.replication</name>
+    <value>2</value>
+  </property>
+  <property>
+    <name>dfs.namenode.name.dir</name>
+    <value>file://$HADOOP_HOME/data/namenode</value>
+  </property>
+  <property>
+    <name>dfs.datanode.data.dir</name>
+    <value>file://$HADOOP_HOME/data/datanode</value>
+  </property>
 </configuration>
 EOT
 
@@ -132,60 +139,61 @@ datanode1
 datanode2
 EOT
 
-echo "=== YARN CONFIG ==="
+# --- YARN CONFIG ---
 cat <<EOT > $HADOOP_HOME/etc/hadoop/mapred-site.xml
 <configuration>
-<property>
-  <name>mapreduce.framework.name</name>
-  <value>yarn</value>
-</property>
+  <property>
+    <name>mapreduce.framework.name</name>
+    <value>yarn</value>
+  </property>
 </configuration>
 EOT
 
 cat <<EOT > $HADOOP_HOME/etc/hadoop/yarn-site.xml
 <configuration>
-<property>
-  <name>yarn.resourcemanager.hostname</name>
-  <value>namenode</value>
-</property>
-<property>
-  <name>yarn.nodemanager.aux-services</name>
-  <value>mapreduce_shuffle</value>
-</property>
+  <property>
+    <name>yarn.resourcemanager.hostname</name>
+    <value>namenode</value>
+  </property>
+  <property>
+    <name>yarn.nodemanager.aux-services</name>
+    <value>mapreduce_shuffle</value>
+  </property>
 </configuration>
 EOT
 
-echo "=== SYNC HADOOP CONFIG TO DATANODES ==="
-for node in $DN1_PRIVATE_IP $DN2_PRIVATE_IP
-do
+# --- SYNC HADOOP CONFIG TO DATANODES ---
+echo "=== SYNC CONFIG TO DATANODES ==="
+for node in $DN1_PRIVATE_IP $DN2_PRIVATE_IP; do
 scp $HADOOP_HOME/etc/hadoop/*.xml $DN_USER@$node:$BASE_HOME/hadoop/etc/hadoop/
 scp $HADOOP_HOME/etc/hadoop/workers $DN_USER@$node:$BASE_HOME/hadoop/etc/hadoop/
 done
 
-echo "=== CONFIGURE SPARK (YARN) ==="
+# --- SPARK CONFIG ---
+echo "=== CONFIGURE SPARK ==="
 cat <<EOT > $SPARK_HOME/conf/spark-defaults.conf
 spark.master yarn
 spark.submit.deploy-mode client
 EOT
 
-echo "=== SYNC SPARK CONFIG ==="
-for node in $DN1_PRIVATE_IP $DN2_PRIVATE_IP
-do
+for node in $DN1_PRIVATE_IP $DN2_PRIVATE_IP; do
 scp $SPARK_HOME/conf/spark-defaults.conf $DN_USER@$node:/opt/spark/conf/
 done
 
+# --- MINIO INSTALL ---
 echo "=== INSTALL MINIO ==="
 wget https://dl.min.io/server/minio/release/linux-amd64/minio
 chmod +x minio
 sudo mv minio /usr/local/bin/
 mkdir -p ~/minio-data
 
+# --- AIRFLOW INSTALL ---
 echo "=== INSTALL AIRFLOW ==="
 python3 -m venv airflow-venv
 source airflow-venv/bin/activate
 pip install --upgrade pip
-
 pip install "apache-airflow==2.9.0" \
   --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-2.9.0/constraints-3.11.txt"
 
 echo "=== SETUP COMPLETE ==="
+echo "You can now start HDFS, YARN, Spark, Airflow, MinIO, and upload your jobs/data."
